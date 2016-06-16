03d8f33cfeda377fb27ac519e93c90487061def6
liujian <382256612@qq.com>
2016-06-16 08:29:59 +0800

    第二章 6/16

---
+++ chapter2/src/main/java/com/liujianhome/service/CustomerService.java
@@ -0,0 +1,69 @@
+package com.liujianhome.service;
+
+import com.liujianhome.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 提供客户数据服务
+ * Created by Administrator on 2016/6/16.
+ */
+public class CustomerService {
+
+    /**
+     * 获取客户列表
+     *
+     * @param keyword
+     * @return
+     */
+    public List<Customer> getCustomerList(String keyword) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * 获取客户
+     *
+     * @param id
+     * @return
+     */
+    public Customer getCustomer(long id) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * 创建客户
+     *
+     * @param fieldMap
+     * @return
+     */
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        // TODO
+        return false;
+    }
+
+    /**
+     * 更新客户
+     *
+     * @param id
+     * @param fieldMap
+     * @return
+     */
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        // TODO
+        return false;
+    }
+
+    /**
+     * 删除客户
+     *
+     * @param id
+     * @return
+     */
+    public boolean deleteCustomer(long id) {
+        // TODO
+        return false;
+    }
+}
---
+++ chapter2/src/test/java/com.liujianhome/CustomerServiceTest.java
@@ -0,0 +1,70 @@
+package com.liujianhome;
+
+import com.liujianhome.model.Customer;
+import com.liujianhome.service.CustomerService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * CustomerService 单元测试
+ * Created by Administrator on 2016/6/16.
+ */
+public class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    public CustomerServiceTest(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // TODO 初始化数据库
+    }
+
+    @Test
+    public void getCustomerListTest() throws Exception {
+        List<Customer> customerList = customerService.getCustomerList("");
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() throws Exception {
+        Customer customer = customerService.getCustomer(1);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception {
+        Map<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("name", "customer99");
+        fieldMap.put("contact", "LJ");
+        fieldMap.put("telephone", "18616228299");
+
+        boolean result = customerService.createCustomer(fieldMap);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception {
+        Map<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("name", "updateName");
+
+        boolean result = customerService.updateCustomer(1, fieldMap);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomerTest() throws Exception {
+        boolean result = customerService.deleteCustomer(1);
+
+        Assert.assertTrue(result);
+    }
+}
